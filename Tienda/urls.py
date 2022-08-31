14c7765afe0c5b9ab342eb895952a633aa7cb426
Lautaro <lautaromigue@gmail.com>
2022-08-30 23:38:13 -0300

    Periferales

--- Tienda/urls.py
+++ Tienda/urls.py
@@ -1,7 +1,7 @@
 from re import search
 from django.urls import path
 
-from Tienda.views import create_game, delete_game, formulario_games, list_games, search_products, update_game, \
+from Tienda.views import Create_peripheral, Delete_peripheral, Detaile_peripheral, List_peripherals, Update_peripheral, create_game, delete_game, formulario_games, list_games, search_products, update_game, \
     Create_phone, List_phones, Delete_phone, Detaile_phone, Update_phone, \
     List_consoles, Detaile_console, Create_console, Delete_console, Update_console, \
     list_products
@@ -22,11 +22,17 @@ urlpatterns = [
     path('update-phone/<int:pk>/',Update_phone.as_view(), name='Update_phone'),
 
 
-    path('list-consoles/', List_consoles.as_view(), name='List_games'),  
+    path('list-consoles/', List_consoles.as_view(), name='List_peripherals'),  
     path('detail-console/<int:pk>/', Detaile_console.as_view(), name='Detaile_console'), 
     path('create-console/', Create_console.as_view(), name= 'Create_console'), 
     path('delete-console/<int:pk>/', Delete_console.as_view(), name= 'Delete_console'), 
     path('update-console/<int:pk>/', Update_console.as_view(), name='Update_console'),
     
-    path('list-products/', list_products, name='list_products')
+    path('list-peripherals/', List_peripherals.as_view(), name='List_peripherals'),  
+    path('detail-peripheral/<int:pk>/', Detaile_peripheral.as_view(), name='Detaile_peripheral'), 
+    path('create-peripheral/', Create_peripheral.as_view(), name= 'Create_peripheral'), 
+    path('delete-peripheral/<int:pk>/', Delete_peripheral.as_view(), name= 'Delete_peripheral'), 
+    path('update-peripheral/<int:pk>/', Update_peripheral.as_view(), name='Update_peripheral'),    
+    
+    path('list-products/', list_products, name='list_products'),
 ]
