fd39e0fef5be30d797c40cc910749b99d334d497
Lautaro <lautaromigue@gmail.com>
2022-09-05 21:02:30 -0300

    Ultimos cambios users 2

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("profile/", profile, name="profile"),
     path("delete-profile/", delete_profile, name="delete_profile")
-    
 ]
